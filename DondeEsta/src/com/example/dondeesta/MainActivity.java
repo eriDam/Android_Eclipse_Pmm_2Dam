3c98f8ed5e437fd9a6fd8c6e7c39f67d42a1bcb2
eriDam <erika.c.caballero@gmail.com>
2014-09-19 01:11:57 +0200

    Project 01. DondeEsta. Completed and running.
    Create strings, add in main activity listener for button and textField.

--- DondeEsta/src/com/example/dondeesta/MainActivity.java
+++ DondeEsta/src/com/example/dondeesta/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.dondeesta;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +16,27 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//creo un nuevo botón y el campo de texto de donde se cogerá la dirección a buscar
+		final Button botonB = (Button) findViewById(R.id.button1);
+		final EditText textField = (EditText)findViewById(R.id.editText1);
+		
+		//Añado un nuevo listener
+		botonB.setOnClickListener(new Button.OnClickListener(){
+		
+
+			@Override
+			public void onClick(View v) {
+				
+				//Para lanzar un nuevo activity, se utiliza la clase Intent
+				Intent mapa = new Intent(
+						android.content.Intent.ACTION_VIEW,
+						Uri.parse("geo:0,0?q= ("+textField.getText()+")")//la direccion la cogerá mediante getText() del campo de texto
+						);
+					startActivity(mapa);		
+			}
+		}
+	);
 	}
 
 	@Override
