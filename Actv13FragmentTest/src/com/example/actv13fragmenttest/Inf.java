f9cdd660d65af4a603407dbf4aa1f95d0adc08b5
eriDam <erika.c.caballero@gmail.com>
2014-10-25 18:32:29 +0200

    Activity 13 FragmentTest
    Create 2 frames sup e inf in LinearLayout

---
+++ Actv13FragmentTest/src/com/example/actv13fragmenttest/Inf.java
@@ -0,0 +1,85 @@
+package com.example.actv13fragmenttest;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Inf extends Fragment {
+
+	private static final String TAG = "Fragment Inf";
+	
+	@Override
+	public void onAttach(Activity activity) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onAttach()");
+		super.onAttach(activity);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onCreate()");
+		super.onCreate(savedInstanceState);
+	}
+
+	/** En un fragmento, usamos onCreateView a diferencia del mainActivity en el que se usa onCreate,  */
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		/** Utilizamos el inflater pasandole un fragment inf como parámetro  */
+		return inflater.inflate(R.layout.fragment_inf, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+	
+	@Override
+	public void onStart() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onStart()");
+		super.onStart();
+	}
+	
+	@Override
+	public void onResume() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onResume()");
+		super.onResume();
+	}
+
+	
+	@Override
+	public void onPause() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onPause()");
+		super.onPause();
+	}
+
+	@Override
+	public void onStop() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onStop()");
+		super.onStop();
+	}
+	
+	@Override
+	public void onDetach() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onDetach()");
+		super.onDetach();
+	}
+
+	
+	@Override
+	public void onDestroy() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onDestroy()");
+		super.onDestroy();
+	}
+
+	@Override
+	public void onDestroyView() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onDestroyView()");
+		super.onDestroyView();
+	}
+
+}
